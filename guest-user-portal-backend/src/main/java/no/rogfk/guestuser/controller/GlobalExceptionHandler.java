0e70ae9086a87d354e49e4646b069004ea62f808
Frode Sjovatsen <frode@sjovatsen.net>
2017-02-14 14:19:33 +0100

    added globalexceptionhandler controller

---
+++ guest-user-portal-backend/src/main/java/no/rogfk/guestuser/controller/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package no.rogfk.guestuser.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+/**
+ * Maps every request spring cannot handle to deliver index.html
+ */
+@ControllerAdvice
+class GlobalExceptionHandler {
+  private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public String handle(Exception ex) {
+    logger.error("Exception caught: NoHandlerFoundException - " + ex.getMessage());
+    return "forward:/index.html";
+  }
+}
